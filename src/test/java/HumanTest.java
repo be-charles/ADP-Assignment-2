c6f23c821cec6b03b65031ae8fd21e303bbbf6b6
ranzve <brandoncharles.freelance@gmail.com>
2020-03-04 00:51:38 +0200

    ADP ASSIGNMENT 3

--- src/test/java/HumanTest.java
+++ src/test/java/HumanTest.java
@@ -1,7 +1,10 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class HumanTest {
 
+    @Test
     public void isMammal(){
         Human h = new Human();
         assertEquals(true, h instanceof Mammal);
